21838cfb3532e45800ca73277e823affdeda70ad
Chahal1706 <cxc161230@utdallas.edu>
2017-07-18 20:02:23 -0500

    Assignment2_started

--- assignment_02/analyze_medicare_data.py
+++ assignment_02/analyze_medicare_data.py
@@ -7,20 +7,21 @@ import openpyxl
 import sqlite3
 import glob
 import getpass
+import pandas as pd
 
 '''This line is initializing the place holder for the url path''' 
 url="https://data.medicare.gov/views/bg9k-emty/files/0a9879e0-3312-4719-a1db-39fd114890f1?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
-print(url)
+#print(url)
 '''this line of code will request the above mentioned url and is getting the response back'''
 r = requests.get(url)
-print(r)
-print(r.content)
+#print(r)
+#print(r.content)
 
 
 '''Now, we are making a staging directory to store the contensts of the zip folder after unzipping it'''
 staging_dir_name = "staging"
 
-"""
+
 os.mkdir(staging_dir_name) 
 
 '''Represents a path relative to the current directory on drive'''
@@ -35,121 +36,89 @@ zf.write(r.content)
 zf.close()
 
 '''zipfile is a function of object zipfile it will have '''
-#z = zipfile.ZipFile(zip_file_name,'r')
+z = zipfile.ZipFile(zip_file_name,'r')
 z.extractall(staging_dir_name)
 z.close() 
 
-"""
 
 k_url = "http://kevincrook.com/utd/hospital_ranking_focus_states.xlsx"
 r = requests.get(k_url)
-xf = open("C:\\Users\\chaha\\Downloads\\hospital_ranking_focus_states.xlsx","wb")
+xf = open("hospital_ranking_focus_states.xlsx","wb")
 xf.write(r.content)
 xf.close()
 
 
 wb = openpyxl.load_workbook("hospital_ranking_focus_states.xlsx")
-for sheet_name in wb.get_sheet_names():
-    print(sheet_name)
-    
+   
 sheet = wb.get_sheet_by_name("Hospital National Ranking") 
-i = 1
-while sheet.cell(row=i, column=1).value != None:
-    print(sheet.cell(row=i, column=1).value, "|" , sheet.cell(row=i, column=2).value)
-    i+=1
-    
+
 
 sheet2 = wb.get_sheet_by_name("Focus States")
-i = 1
-while sheet.cell(row=i, column=1).value != None:
-    print(sheet2.cell(row=i, column=1).value, "|" , sheet2.cell(row=i, column=2).value)
-    i+=1
-    
-wb2 = openpyxl.Workbook()
-sheet_1 = wb2.create_sheet("utd")
-sheet_1.cell(row=1,column=1,value="buan")
-for i in range(2,11):
-    sheet_1.cell(row=i,column=1,value=i-1)
+
+
+
     
     
-sheet_2 = wb2.create_sheet("test")
-sheet_2.cell(row=1,column=2,value="valued")
-wb2.remove_sheet(wb2.get_sheet_by_name('Sheet'))
-wb2.save("test.xlsx")
-wb2.close()
-
-conn = sqlite3.connect("test.db")
-c1 = conn.cursor()
-sql_str = "drop table if exists my_table"
-c1.execute(sql_str)
-
-sql_str = """
-create table if not exists my_table (
-column_1 text,
-column_2 text,
-column_3 text
-)
-"""
-
-c1.execute(sql_str)
-sql_str = "insert into my_table (column_1,column_2,column_3) values (?,?,?)"
-sql_tuple=('a','b','c')
-c1.execute(sql_str,sql_tuple)
-conn.commit()
 
 
-sql_str = "select * from my_table"
-rows = c1.execute(sql_str)
-for row in rows :
-    print(row)
-    
-sql_str = "select * from sqlite_master"
-rows = c1.execute(sql_str)
-for row in rows :
-    print(row)
-    
-sql_str = "PRAGMA table_info('my_table')"
-rows = c1.execute(sql_str)
-for row in rows :
-    print(row)
-    
-
-
-sql_str = "select * from sqlite_master where tbl_name = ?"
-sql_tuple = ('my_table',)
-c1.execute(sql_str,sql_tuple)
-for row in rows :
-    print(row)
-
-sql_str = "select count(*) from my_table"
-rows = c1.execute(sql_str)
-for row in rows :
-    print(row)
-
-fn =  os.path.join(staging_dir_name, "Timely and Effective Care - Hospital.csv")
-in_fp = open(fn,"rt",encoding='cp1252')#rt = read text
-input_data = in_fp.read()
-in_fp.close()
-
-ofn =  os.path.join(staging_dir_name, "Timely and Effective Care - Hospital.csv.fix")
-out_fp = open(ofn,"wt",encoding='utf-8')# switching from codepage 1252 to utf-8 and removing nulls
-for c in input_data:
-    if c != '\0':
-        out_fp.write(c)
-out_fp.close()
-
-
-# get the zip file unzip and loop through the files,create tables from csv files with the replacements and insert the data into table
-#check through db browser
-#code to convert from cp1253 to utf-8 
-#use numpi for sd(population),median,mode
-#create the output excel files as shown above
 glob_dir = os.path.join(staging_dir_name,"*.csv")
-for file_name in glob.glob(glob_dir):
-    print (file_name)
-    print("    basename:",os.path.basename(file_name))#basename is filename without the path
-    print("    split extension:",os.path.splitext(os.path.basename(file_name)))
-    print("    dirname:",os.path.dirname(file_name))
-    print("    absolute path:",os.path.abspath(file_name))
+#for file_name in glob.glob(glob_dir):
     
-c1.close() 
+    
+    
+#Importing csv to sqlite server
+
+  
+                      
+def create_tables():
+    try:
+        conn = sqlite3.connect("medicare_hospital_compare.db")
+        glob_dir = os.path.join(staging_dir_name, "*.csv")
+        for file_name in glob.glob(glob_dir):
+            absolute_path = os.path.abspath(file_name)
+            column_name=[]
+            try:
+                df= pd.read_csv(absolute_path)
+                column_name = list(df.columns)
+                dict_types = {x:'str' for x in column_name}
+                df = pd.read_csv(absolute_path,dtype=dict_types)
+            except UnicodeDecodeError:
+                in_fp = open(absolute_path, "rt", encoding='cp1252')  
+                input_data = in_fp.read()
+                in_fp.close()
+                n_file_name = absolute_path + ".fix"
+                out_fp = open(n_file_name, "wt", encoding='utf-8')
+                for c in input_data:
+                    if c != '\0':
+                        out_fp.write(c)
+                out_fp.close()
+                df = pd.read_csv(n_file_name)
+                col = list(df.columns)
+                dict_types = {x:'str' for x in column_name}
+                df = pd.read_csv(n_file_name,dtype=dict_types)
+            column_name = list(df.columns)
+            col_names_new = []
+            for item in column_name:
+                temp = item.lower().replace(" ", "_").replace("-", "_").replace("%", "pct").replace("/", "_")
+                if(not temp[0].isalpha()):
+                    col_names_new.append("c_" + temp)
+                else:
+                    col_names_new.append(temp)
+            df.columns = col_names_new
+            tablename = os.path.splitext(os.path.basename(file_name))[0].lower().replace(" ","_").replace("-","_").replace("%", "pct").replace("/","_")
+            if(not tablename[0].isalpha()):
+                tablename = "t_" + tablename
+            df.to_sql(tablename,con=conn,if_exists='fail',dtype = {col:'text' for col in df}, index=False)
+           
+    ## Raises exception on unable to establish connection
+    except ConnectionError:
+        print("Unable to get database connection.. Exiting out of program")
+        raise SystemExit
+    ## Closes connection upon running the query
+    finally:
+        conn.close()
+    ##Enables garbage collection
+    #gc.collect()
+    return
+
+create_tables() 
