822d49ecb17eee62d819b7fb18d83f54d9851b95
Chahal1706 <cxc161230@utdallas.edu>
2017-07-20 22:54:22 -0500

    Update analyze_medicare_data.py

--- assignment_02/analyze_medicare_data.py
+++ assignment_02/analyze_medicare_data.py
@@ -122,3 +122,164 @@ def create_tables():
     return
 
 create_tables() 
+
+'''
+the below block of code is performing the following functions:
+1) making connection to the db
+2) fetching the data from hospital_general_information
+3) putting each row as dictionary to list list_of_rows_from_table
+'''
+conn = sqlite3.connect("medicare_hospital_compare.db")
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
+rows = c.execute('SELECT * FROM hospital_general_information')
+
+list_of_rows_from_table=list()
+for row in rows:
+    list_of_rows_from_table.append(dict(row))
+
+
+    
+  
+'''
+the below piece of code performs the following function:
+1) reads the hospital_ranking_focus_state file
+2) creates a dataframe  : df_hospital_national_ranking which contains data from sheet : Hospital National Ranking
+3) crates a dataframe : df_focus_states which contains data from sheet : Focus States'
+
+'''  
+
+df_rows_table = pd.DataFrame(list_of_rows_from_table)
+xls_file = pd.ExcelFile('hospital_ranking_focus_state.xlsx')
+df_hospital_national_ranking=xls_file.parse('Hospital National Ranking')
+df_focus_states=xls_file.parse('Focus States')
+#print(df_focus_states[:5])
+#print(df_hospital_national_ranking[:5])
+
+'''
+we filter out the first rows from the ranking dataframe
+'''
+
+
+df_hospital_rank_100=df_hospital_national_ranking[:100].copy()
+#print(df_hospital_rank_100)
+
+#df_hospital_rank_100['hospital_name']=df_hospital_rank_100['Provider ID'].map(df_rows_table)
+#print(any(df_rows_table.provider_id=='450023'))
+
+
+
+#print(df_hospital_rank_100_merged)
+#print(any(df_hospital_rank_100['Provider ID']==450023))
+
+#print(df_hospital_rank_100.dtypes)
+
+'''
+we convert the data type of provider id to int64 so that we can perform the merge operation below.
+'''
+df_rows_table['provider_id']=df_rows_table['provider_id'].astype(np.int64)
+#df_rows_table.dtypes
+
+'''
+the below line is equivalant to a join between the data from :
+1) df_hospital_rank_100
+2) df_rows_table
+
+we do this because we need the values of Hospital Namne , City, State and county name from second table.
+'''
+
+df_hospital_rank_100_merged=pd.merge(left=df_hospital_rank_100,right=df_rows_table,left_on='Provider ID',right_on='provider_id')
+#print(df_hospital_rank_100_merged)
+'''
+we rename the column in the dataframe according to the requirement mentioned below.
+'''
+
+df_hospital_rank_100_merged.rename(columns={'hospital_name':'Hospital Name','city':'City','state':'State','county_name':'County'},inplace=True)
+
+#print(df_hospital_rank_100_merged[['Provider ID','Hospital Name','City','State','County']])
+'''
+we copy the columns required into a staging dataframe.
+'''
+
+df_hospital_rank_100_merged_filtered=df_hospital_rank_100_merged[['Provider ID','Hospital Name','City','State','County']].copy()
+
+'''
+we remove the automatically created indexed column by setting the Provider ID  as the index
+'''
+
+
+df_hospital_rank_100_merged_filtered.set_index('Provider ID',inplace=True)
+#print(df_hospital_rank_100_merged_filtered)
+
+
+
+
+
+#print("before sorting: ",df_focus_states)
+'''
+we sort the below dataframe by state name
+'''
+
+df_focus_states.sort_values(by='State Name',inplace=True)
+#print(df_focus_states)
+
+'''
+we create a dictionary from the above data frame.
+'''
+
+dict_of_state_abbr_sorted = dict(zip(df_focus_states['State Name'],df_focus_states['State Abbreviation']))
+
+'''
+we create an ExcelWriter object - hospital_ranking.xlsx file.
+we create a sheet : Nationwide where we store our filtered dataframe.
+
+'''
+
+writer=pd.ExcelWriter('hospital_ranking.xlsx')
+df_hospital_rank_100_merged_filtered.to_excel(writer,'Nationwide')
+
+'''
+we will now iterate through the key, value pair 
+'''
+for key,value in dict_of_state_abbr_sorted.items():
+    
+    '''
+    we filter the rows by state name from df_rows_table
+    '''
+    
+    df_rows_table_filter_by_state_name=df_rows_table[(df_rows_table.state==value)].copy()
+    '''
+    we create an inner join and get eht common data from ranking and the df_rows_table. 
+    '''
+     
+    df_hospital_filtered_by_state_name_merged=pd.merge(left=df_hospital_national_ranking,right=df_rows_table_filter_by_state_name,left_on='Provider ID',right_on='provider_id')
+    
+    '''
+    we rename the columns according to the requirement
+    '''
+    #df_hospital_filtered_by_state_name_merged.set_index('Provider ID',inplace=True)
+    df_hospital_filtered_by_state_name_merged.rename(columns={'hospital_name':'Hospital Name','city':'City','state':'State','county_name':'County'},inplace=True)
+    #df_hospital_filtered_by_state_name_merged_columns_filtered=df_hospital_filtered_by_state_name_merged[['Provider ID','Hospital Name','City','State','County']].copy()
+    '''
+    sort the data obtained per state by Ranking Column.
+    '''
+    df_hospital_filtered_by_state_name_merged.sort_values(by='Ranking',inplace=True)
+    #print(df_hospital_filtered_by_state_name_merged)
+    '''
+    we then filter out the required column and se the Provider ID as the key
+    '''
+    df_hospital_filtered_by_state_name_merged_columns_filtered=df_hospital_filtered_by_state_name_merged[['Provider ID','Hospital Name','City','State','County']].copy()
+    df_hospital_filtered_by_state_name_merged_columns_filtered.set_index('Provider ID',inplace=True)
+    
+    '''
+    we write the data to a new sheet in the excel file
+    '''
+    df_hospital_filtered_by_state_name_merged_columns_filtered.to_excel(writer,key)
+    #break
+
+
+    
+'''
+we save the file.
+'''
+writer.save()
