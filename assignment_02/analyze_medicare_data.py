bd2285930015b808ec0fd9d138d512b835f71fdb
Chahal1706 <cxc161230@utdallas.edu>
2017-07-22 21:00:37 -0500

    Update analyze_medicare_data.py

--- assignment_02/analyze_medicare_data.py
+++ assignment_02/analyze_medicare_data.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[2]:
+
 '''We are first importing all the important methods required for the assignment'''
 
 import requests
@@ -143,17 +148,15 @@ list_of_rows_from_table=list()
 for row in rows:
     list_of_rows_from_table.append(dict(row))
 
-conn.close()
 
 
-    
+conn.close()   
   
 '''
 the below piece of code performs the following function:
 1) reads the hospital_ranking_focus_state file
 2) creates a dataframe  : df_hospital_national_ranking which contains data from sheet : Hospital National Ranking
 3) crates a dataframe : df_focus_states which contains data from sheet : Focus States'
-
 '''  
 
 df_rows_table = pd.DataFrame(list_of_rows_from_table)
@@ -199,7 +202,6 @@ df_rows_table['provider_id']=df_rows_table['provider_id'].astype(np.int64)
 the below line is equivalant to a join between the data from :
 1) df_hospital_rank_100
 2) df_rows_table
-
 we do this because we need the values of Hospital Namne , City, State and county name from second table.
 '''
 
@@ -247,7 +249,6 @@ dict_of_state_abbr_sorted = dict(zip(df_focus_states['State Name'],df_focus_stat
 '''
 we create an ExcelWriter object - hospital_ranking.xlsx file.
 we create a sheet : Nationwide where we store our filtered dataframe.
-
 '''
 
 writer=pd.ExcelWriter('hospital_ranking.xlsx')
@@ -295,9 +296,200 @@ for key,value in dict_of_state_abbr_sorted.items():
 
     
 '''
-we save the file.
+we save the file. this finishes the file : hospital_ranking.xlsx
 '''
 writer.save()
 
 
+'''
+we will now prepare the measure_statistics.xlsx
+'''
+
+'''
+we create a connection to the database and get the data from the timely_and_effective_care__hospital
+'''
+
+conn = sqlite3.connect("medicare_hospital_compare.db")
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
+rows = c.execute('SELECT state,measure_id,measure_name,score FROM timely_and_effective_care___hospital')
+
+list_of_rows_from_timely_and_effective_data_table=list()
+'''
+we are creating a list from the rows of the table with each row as dictionaries.
+'''
+for row in rows:
+    #print(row)
+    list_of_rows_from_timely_and_effective_data_table.append(dict(row))
+
 conn.close()
+'''
+we are creating a dataframe from the above list to perform the aggregations operations.
+'''
+
+df_rows_timely_and_effective_care__hospital_table = pd.DataFrame(list_of_rows_from_timely_and_effective_data_table)
+
+#print(df_rows_timely_and_effective_care__hospital_table[:5])
+
+'''
+We are getting unique measure id
+'''
+
+set_of_measure_id=set(df_rows_timely_and_effective_care__hospital_table['measure_id'])
+
+#print(set_of_measure_id)
+
+
+list_of_data_frames=list()
+
+'''
+we are forming list which are data frames corresponding to each measure id
+'''
+
+for measure_id in set_of_measure_id:
+    #if measure_id == 'PC_01':
+    #print(measure_id)
+    df_rows_filtered_measure_id=df_rows_timely_and_effective_care__hospital_table[(df_rows_timely_and_effective_care__hospital_table.measure_id==measure_id)].copy()
+    df_rows_filtered_measure_id_only_numerical=df_rows_filtered_measure_id[(df_rows_filtered_measure_id['score'].apply(lambda x : x.isdigit() == True))]
+    list_of_data_frames.append(df_rows_filtered_measure_id_only_numerical)
+#print(len(list_of_data_frames))
+
+'''
+we get a dataframe with all the measureid's
+'''
+resultant_data_frame=pd.concat(list_of_data_frames)
+#print(resultant_data_frame[:500])
+'''
+we again get the unique measure id's as some of the measureid might be filtered in the previous step
+'''
+final_set_of_measure_id=set(resultant_data_frame['measure_id'])
+#print(final_set_of_measure_id)
+
+'''
+we convert the data type of the score column to int64
+'''
+
+resultant_data_frame['score']=resultant_data_frame['score'].astype(np.int64)
+
+#measures_statistics.xlsx
+list_nationwide=[]
+
+'''
+this loop we are performing the following operation
+1) Getting the rows from the main data frame filtered by the Measure Id
+2) for each measure id , we calculate the minimum, maximum, average and standard deviation 
+3) we then add them to the list_nationwide that we created above
+'''
+for meid in final_set_of_measure_id:
+    #print(meid)
+    dictionay_of_nationwide_main=dict()
+    df_rows_filtered_meid=resultant_data_frame[(resultant_data_frame.measure_id==meid)].copy()
+    #print(meid)
+    dictionay_of_nationwide_main['Measure ID']=meid
+    dictionay_of_nationwide_main['Measure Name']=list(df_rows_filtered_meid['measure_name'])[0]
+    #print(list(df_rows_filtered_meid['measure_name'])[0])
+    #print(df_rows_filtered_meid['score'].dtype)
+    dictionay_of_nationwide_main['Minimum']=(df_rows_filtered_meid['score']).min()
+    dictionay_of_nationwide_main['Maximum']=(df_rows_filtered_meid['score']).max()
+    dictionay_of_nationwide_main['Average']=(df_rows_filtered_meid['score']).mean()
+    dictionay_of_nationwide_main['Standard Deviation']=(df_rows_filtered_meid['score']).std()
+    #print((df_rows_filtered_meid['score']).min())
+    #print((df_rows_filtered_meid['score']).max())
+    #print((df_rows_filtered_meid['score']).mean())
+    #print((df_rows_filtered_meid['score']).std())
+    #print(dictionay_of_nationwide_main)
+    list_nationwide.append(dictionay_of_nationwide_main)
+    
+    
+#print(list_nationwide)
+'''
+We create a main dataframe from the list that we created above and add appropriate column names.
+'''
+
+nationwide_main_dataframe=pd.DataFrame(list_nationwide, columns=['Measure ID', 'Measure Name', 'Minimum','Maximum','Average','Standard Deviation'])
+'''
+we sort the value by the column Measure ID
+'''
+nationwide_main_dataframe.sort_values(by='Measure ID',inplace=True)
+#print(nationwide_main_dataframe)
+
+'''
+we make the column Measure ID as the index ccolumn , this is to prevent the index appearing in the sheet.
+'''
+nationwide_main_dataframe.set_index('Measure ID',inplace=True)
+
+
+
+#print(resultant_data_frame)
+
+'''
+we then create the ExcelWriter for the measure_statistics.xlsx.
+'''
+
+writer=pd.ExcelWriter('measures_statistics.xlsx')
+'''
+we then create a sheet "Nationwide" and write the nationwide_main_dataframe in that sheet
+we do not close the writer yet
+'''
+nationwide_main_dataframe.to_excel(writer,'Nationwide')
+
+'''
+we then get the key, value from the dict_of_state_abbr_sorted dictionaries and execute the loop as below
+'''
+
+for key,value in dict_of_state_abbr_sorted.items():
+    #print(key)
+    #rint(value)
+    ''''''
+    list_of_state_wise_aggregated_dictionaries=[]
+    dict_of_state_wise_aggregation=dict()
+    '''
+    we filter the rows by state name from resultant_data_frame
+    '''
+    resultant_data_frame_filter_by_state_name=resultant_data_frame[(resultant_data_frame.state==value)].copy()
+    
+    '''
+    we then perform the aggregation by score group by measureid , we get the following aggregations
+    1) Mean
+    2) Minimum
+    3) Maximum
+    4) Standard Deviation
+    '''
+    df_resultant_groupby=pd.DataFrame(resultant_data_frame_filter_by_state_name.groupby(['measure_id'])['score'].min())
+    df_resultant_groupby['Maximum']=pd.DataFrame(resultant_data_frame_filter_by_state_name.groupby(['measure_id'])['score'].max())
+    df_resultant_groupby['Average']=pd.DataFrame(resultant_data_frame_filter_by_state_name.groupby(['measure_id'])['score'].mean())
+    df_resultant_groupby['Standard Deviation']=pd.DataFrame(resultant_data_frame_filter_by_state_name.groupby(['measure_id'])['score'].std())
+    df_resultant_groupby.reset_index(inplace=True)
+    #print(df_resultant_groupby)
+    '''
+    we then rename the score and measure_id column according to the specification mentioned.    
+    '''
+    df_resultant_groupby.rename(columns={'score':'Minimum','measure_id':'Measure ID'},inplace=True)
+    '''
+    we then sort the dataframe by Measure ID
+    '''
+    df_resultant_groupby.sort_values(by='Measure ID',inplace=True)
+    df_resultant_groupby.set_index('Measure ID',inplace=True)
+    '''
+    We then write the dataframe to a separate sheet and name that sheet by the key.
+    '''
+    df_resultant_groupby.to_excel(writer,key)
+    #print(df_resultant_groupby[:5])
+    #print(resultant_data_frame_filter_by_state_name)
+    #break
+
+
+'''
+we now close the writer.
+'''
+
+writer.save()
+
+#df_rows_timely_and_effective_care__hospital_table['score'].apply(lambda x: x.isdigit)
+
+'''
+we now close the connection to the database just in case some connection might be open
+'''
+
+conn.close()
+
